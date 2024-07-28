d2fe41ca8bf7a14731bd81bb532034e092714aad
Vatsala Gupta <vatsala00000gupta@gmail.com>
2024-07-28 11:36:06 +0530

    recursion

---
+++ Recursion/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        println(1, n);
+    }
+
+    public static void println(int i, int n) {
+        if (i > n) {
+            return;
+        }
+        System.out.print("vatsala" + " ");
+        println(i + 1, n);
+    }
+}
